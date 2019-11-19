fd65dd70ec99973a1cec0d948e5fd7ad14ed74d7
Nick Monaco <nickjustpwnedu@gmail.com>
2019-11-19 15:17:11 -0600

    Changes from class on 11-19-2019

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -27,5 +27,43 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+
+        public List<Movie> Search(string term) {
+            List<Movie> results = new List<Movie>();
+
+            foreach(Movie mov in movies) {
+                if (mov.Title.Contains(term, StringComparison.OrdinalIgnoreCase)
+                    || mov.Director != null && mov.Director.Contains(term, StringComparison.OrdinalIgnoreCase)) {
+                    results.Add(mov);
+                }
+            }
+
+
+            return results;
+        }
+
+        public List<Movie> FilterByMPAA(List<Movie> movList, List<string> filters) {
+            List<Movie> results = new List<Movie>();
+
+            foreach(Movie mov in movList) {
+                if (filters.Contains(mov.MPAA_Rating)) {
+                    results.Add(mov);
+                }
+            }
+
+            return results;
+        }
+
+        public List<Movie> FilterByMinIMDB(List<Movie> movList, float min) {
+            List<Movie> results = new List<Movie>();
+
+            foreach(Movie mov in movList) {
+                if(mov.IMDB_Rating != null && mov.IMDB_Rating >= min) {
+                    results.Add(mov);
+                }
+            }
+
+            return results;
+        }
     }
 }
--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,10 +9,39 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
+        public MovieDatabase MovieDatabase = new MovieDatabase();
 
-        public void OnGet()
-        {
+        public List<Movie> Movies;
 
+        [BindProperty]
+        public string search { get; set; } = null;
+
+        [BindProperty]
+        public List<string> mpaa { get; set; } = new List<string>();
+
+        [BindProperty]
+        public float? minIMDB { get; set; } = null;
+
+
+        public void OnGet() {
+            Movies = MovieDatabase.All;
+        }
+
+        public void OnPost() {
+            if(search != null && mpaa.Count > 0) {
+                Movies = MovieDatabase.Search(search);
+                Movies = MovieDatabase.FilterByMPAA(Movies, mpaa);
+            } else if(search != null){
+                Movies = MovieDatabase.Search(search);
+            } else if(mpaa.Count > 0){
+                Movies = MovieDatabase.FilterByMPAA(MovieDatabase.All, mpaa);
+            } else {
+                Movies = MovieDatabase.All;
+            }
+
+            if(minIMDB is float r) {
+                Movies = MovieDatabase.FilterByMinIMDB(Movies, r);
+            }
         }
     }
 }
