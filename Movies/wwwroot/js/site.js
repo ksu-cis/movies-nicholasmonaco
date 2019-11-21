42212f2e93b4bce3dd794d8d30b7c01848759d69
Nick Monaco <nickjustpwnedu@gmail.com>
2019-11-21 15:27:18 -0600

    Changes from class (ajax)

--- Movies/wwwroot/js/site.js
+++ Movies/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var form = document.getElementById("search-form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var inputs = document.getElementsByName("input");
+
+    var formData = new FormData();
+
+    for (var i = 0; i < inputs.length; i++) {
+        var input = inputs[i];
+        if (input.type == 'checkbox') {
+            if (input.checked) { formData.append(input.name, input.value); }
+        } else {
+            formData.set(input.name, input.value);
+        }
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/', true);
+    xhr.setRequestHeader("RequestVerificationToken", token);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status == 200) {
+            console.log(xhr.responseText);
+        }
+    }
+    xhr.send(formData);
+});
